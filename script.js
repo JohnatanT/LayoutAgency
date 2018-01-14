0a75d9ca5f4b84e51a21fcdac59daf3bec370baf
Johnatan Tavares <johnatan12014@hotmail.com>
2018-01-14 16:22:26 -0200

    Template criado

---
+++ script.js
@@ -0,0 +1,9 @@
+$('#ancora').on('click', function(e) {
+    e.preventDefault();
+    var id = $(this).attr('href'),
+            targetOffset = $(id).offset().top;
+            
+    $('html, body').animate({ 
+        scrollTop: targetOffset - 100
+    }, 500);
+});
